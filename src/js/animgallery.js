d579ca9b3cb906bf32375b0f7026935e4844002e
unknown <potapenko.lilia98@gmail.com>
2021-03-18 20:39:59 +0200

    Grid

--- src/js/animgallery.js
+++ src/js/animgallery.js
@@ -1,5 +1,11 @@
 export default function (){
-    const imgCont = document.querySelectorAll(".img_container");
+    visibilityOfGallery();
+    modalOfGallery();
+}
+
+const imgCont = document.querySelectorAll(".img_container");
+
+const visibilityOfGallery = () => {
     window.addEventListener('scroll', ()=>{
         imgCont.forEach(item =>{
             if(item.getBoundingClientRect().top - window.innerHeight <= 0){
@@ -7,15 +13,19 @@ export default function (){
             }
         });
     });
+}
 
+const modalOfGallery = () =>{
     const modal = document.querySelector(".hidden_modal");
     const modalImg = document.getElementById("modal_img");
+
     imgCont.forEach((item, key) =>{
         item.addEventListener('click', ()=>{
             modal.classList.add("visible_modal");
             modalImg.src =`src/img/photo1${key}.jpg`;
         });
     });
+
     modal.addEventListener('click', ()=>{
         modal.classList.remove("visible_modal");
     });
