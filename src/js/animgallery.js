2cf74753a5e0575d33606a06f475be373c5bc21b
unknown <potapenko.lilia98@gmail.com>
2021-03-18 17:49:44 +0200

    Anomate gallery with modal

---
+++ src/js/animgallery.js
@@ -0,0 +1,22 @@
+export default function (){
+    const imgCont = document.querySelectorAll(".img_container");
+    window.addEventListener('scroll', ()=>{
+        imgCont.forEach(item =>{
+            if(item.getBoundingClientRect().top - window.innerHeight <= 0){
+                item.classList.add("img_visible");
+            }
+        });
+    });
+
+    const modal = document.querySelector(".hidden_modal");
+    const modalImg = document.getElementById("modal_img");
+    imgCont.forEach((item, key) =>{
+        item.addEventListener('click', ()=>{
+            modal.classList.add("visible_modal");
+            modalImg.src =`src/img/photo1${key}.jpg`;
+        });
+    });
+    modal.addEventListener('click', ()=>{
+        modal.classList.remove("visible_modal");
+    });
+}
--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,10 @@
 
 import burger from './burger.js';
 import accordion from './accordion.js';
+import animgallery from './animgallery.js';
 
 window.addEventListener('load', ()=>{
     burger();
     accordion();
+    animgallery();
 });
