9236902ac5b84a2be523db0b93bf8c6d4deb0ab6
unknown <potapenko.lilia98@gmail.com>
2021-03-23 15:01:34 +0200

    Grid with fetch

---
+++ src/js/fetchgrid.js
@@ -0,0 +1,35 @@
+export default function(){
+    fetch("https://jsonplaceholder.typicode.com/comments")
+    .then((response) =>{
+        return response.json();
+    })
+    .then((data)=>{
+        inpData(data);
+    })
+    .catch((err)=>{
+        console.log("API has no access\n"+ err);
+    });
+}
+
+const inpData = (data)=>{
+    const namePost = document.querySelectorAll(".name");
+    const mailPost = document.querySelectorAll(".mail");
+    const bodyPost = document.querySelectorAll(".post");
+
+    namePost.forEach((item, key) =>{
+        item.innerHTML = data[key].name;
+    });
+    mailPost.forEach((item, key) =>{
+        item.innerHTML = data[key].email;
+        item.href = `mailto:${data[key].email}`;
+    });
+    bodyPost.forEach((item, key) =>{
+        for(let i=0; i<data[key].body.length; i++){
+            if(i===0){
+                item.innerHTML = data[key].body[i].toUpperCase();
+            } else{
+                item.innerHTML += data[key].body[i];
+            }
+        }
+    });
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,11 @@
-
-// AOS.init();
-
 import burger from './burger.js';
 import accordion from './accordion.js';
 import animgallery from './animgallery.js';
+import fetchgrid from './fetchgrid.js';
 
 window.addEventListener('load', ()=>{
     burger();
     accordion();
     animgallery();
+    fetchgrid();
 });
